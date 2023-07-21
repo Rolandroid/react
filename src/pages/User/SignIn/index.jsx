90b697406bba2a3b514299bb8b2a31200bc9007d
rolandroid <r.lisarazo@gmail.com>
2023-07-21 16:14:32 -0300

    min 50

--- src/pages/User/SignIn/index.jsx
+++ src/pages/User/SignIn/index.jsx
@@ -12,6 +12,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Copyright from '../Copyright';
+import { Formik, validateYupSchema } from 'formik';
 
 
 
@@ -20,82 +21,121 @@ import Copyright from '../Copyright';
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Ingresá
+                    </Typography>
+
+                    <Formik
+                        initialValues={{
+                            name: [],
+                            password: [],
+                        }}
+                        validate={(values) => {
+                            const errors = {};
+                            const regexpEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+                            if (!values.email) {
+                                errors.email = "Email requerido";
+                            } else if (!regexpEmail.test(values.email)) {
+                                errors.email = "Email inválido";
+                            }
+
+                            if (!values.name) {
+                                errors.name = "Nombre requerido"
+                            }
+
+                            return errors;
+                        }}
+                        onSubmit={(values, { setSubmiting }) => {
+                            console.log(values)
+
+                            setSubmiting(false)
+                        }}
+
+                    >
+                        {
+                            ({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting
+                            }) => (
+                                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                                    <TextField
+                                        margin="normal"
+                                        fullWidth
+                                        name="name"
+                                        label="Nombre"
+                                        type="text"
+                                        id="name" 
+                                        autoFocus
+                                        values={values.name}
+                                        error={errors.name && touched.name}
+                                        onChange = {handleChange}
+                                        onBlur={handleBlur}
+                                        helperText={errors.name && touched.name && errors.name}
+                                    />
+
+                                    <TextField
+                                        margin="normal"
+                                        fullWidth
+                                        name="email"
+                                        label="Email"
+                                        type="text"
+                                        id="email" 
+                                        values={values.email}
+                                        error={errors.email && touched.email}
+                                        onChange = {handleChange}
+                                        onBlur={handleBlur}
+                                        helperText={errors.email && touched.email && errors.email}
+                                    />
+
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        sx={{ mt: 3, mb: 2 }}
+                                    >
+                                        Iniciar
+                                    </Button>
+                                    <Grid container>
+                                        
+                                        <Grid item>
+                                            <Link href="#" variant="body2">
+                                                {"Don't have an account? Sign Up"}
+                                            </Link>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            )
+                        }
+
+
+
+
+                    </Formik>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
+    );
 }
